a762987cb25cdef364399c968d771a20db596a4f
Cyrus Jose <cyrus.jose@outlook.com>
2020-09-20 11:32:09 -0700

    DRY

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -148,69 +148,31 @@ function populateChart() {
   });
 }
 
-function sendTransaction(isAdding) {
-  let nameEl = document.querySelector("#t-name");
-  let amountEl = document.querySelector("#t-amount");
-  let errorEl = document.querySelector(".form .error");
+sendTransaction = isAdding => {
+  if (!transactionForm.validation()) return;
+  // Create a record
+  const transaction = transactionForm.transaction();
+  // condition for subtracting funds
+  if (!isAdding) transaction.value *= -1;
 
-  // validate form
-  if (nameEl.value === "" || amountEl.value === "") {
-    errorEl.textContent = "Missing Information";
-    return;
-  } else {
-    errorEl.textContent = "";
-  }
-
-  // create record
-  let transaction = {
-    name: nameEl.value,
-    value: amountEl.value,
-    date: new Date().toISOString()
-  };
-
-  // if subtracting funds, convert amount to negative number
-  if (!isAdding) {
-    transaction.value *= -1;
-  }
-
-  // add to beginning of current array of data
+  // Add to the beginning of current array
   transactions.unshift(transaction);
 
-  // re-run logic to populate ui with new record
+  // re-run logix to populate ui with new record.
   populateChart();
   populateTable();
   populateTotal();
 
-  // also send to server
-  fetch("/api/transaction", {
-    method: "POST",
-    body: JSON.stringify(transaction),
-    headers: {
-      Accept: "application/json, text/plain, */*",
-      "Content-Type": "application/json"
+  transactionAPI.create(transaction).then(data => {
+    if (data.errors) {
+      transactionForm.showError("Missing Information");
+    } else {
+      transactionForm.clear();
     }
-  })
-    .then(response => {
-      return response.json();
-    })
-    .then(data => {
-      if (data.errors) {
-        errorEl.textContent = "Missing Information";
-      } else {
-        // clear form
-        nameEl.value = "";
-        amountEl.value = "";
-      }
-    })
-    .catch(err => {
-      // fetch failed, so save in indexed db
-      saveRecord(transaction);
-
-      // clear form
-      nameEl.value = "";
-      amountEl.value = "";
-    });
-}
+  }).catch(() => {
+    transactionForm.clear();
+  });
+};
 
 document.querySelector("#add-btn").onclick = function () {
   sendTransaction(true);
