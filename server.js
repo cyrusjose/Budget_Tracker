6b49015e0dd17bd96404794ab8feed95bc492cc2
Cyrus Jose <cyrus.jose@outlook.com>
2020-09-20 13:37:10 -0700

    fixing server.js

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
-require("dotenv").config();
-
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
 const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
